583a1eddf7b88f4f85d8789ac3e2589cdce777ad
Admin <alexandr.erohin.00@mail.ru>
2022-03-23 12:04:24 +0700

    pz4

---
+++ PZ4/PZ4/Chair.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ4
+{
+    internal class Chair
+    {
+        public Chair()
+        {
+
+        }
+        private int Height;
+        private int Leg;
+        private string Material;
+
+        public Chair(int Height, int Leg, string Material)
+        {
+            if (Height >= 20 && Height <= 60)
+            {
+                this.Height = Height;
+            }
+            else
+            {
+                Console.WriteLine("Не входит в диапазон");
+            }
+
+            if (Leg == 1 || Leg == 2 || Leg == 4)
+            {
+                this.Leg = Leg;
+            }
+            else
+            {
+                Console.WriteLine("Не входит в диапазон");
+            }
+
+            if (Material == "Девево" || Material == "Пластик" || Material == "Металл")
+            {
+                this.Material = Material;
+            }
+            else
+            {
+                Console.WriteLine("Материал отсутствует либо не существует");
+            }
+        }
+
+
+
+        public void SetHeight(int Height)
+        {
+            if (Height >= 20 && Height <= 60)
+            {
+                this.Height = Height;
+            }
+            else
+            {
+                Console.WriteLine("Не входит в диапазон");
+            }
+        }
+        public void PrintHeight()
+        {
+            Console.WriteLine("Высота сидушки = " + Height + " см");
+        }
+
+
+        public void SetLeg(int Leg)
+        {
+            if (Leg == 1 || Leg == 2 || Leg == 3)
+            {
+                this.Leg = Leg;
+            }
+            else
+            {
+                Console.WriteLine("Не входит в диапазон");
+            }
+        }
+        public void PrintLeg()
+        {
+            Console.WriteLine("Количество ножек = " + Leg);
+        }
+
+
+        public void GetMaterial(string Material)
+        {
+            if (Material == "Дерево" || Material == "Пластик" || Material == "Металл")
+            {
+                this.Material = Material;
+            }
+            else
+            {
+                Console.WriteLine("Материал отсутствует либо не существует");
+            }
+        }
+        public void PrintMaterial()
+        {
+            Console.WriteLine("Материал изделия = " + Material);
+        }
+
+
+
+
+
+    }
+}
---
+++ PZ4/PZ4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chair first = new Chair();
+            first.SetHeight(20);
+            first.PrintHeight();
+            first.SetLeg(2);
+            first.PrintLeg();
+            first.GetMaterial("Дерево");
+            first.PrintMaterial();
+            Console.ReadLine();
+        }
+    }
+}
